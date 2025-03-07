054f22f3146d8fe70b152ecd94ddc05c2aaf242c
Vojtěch Vašmucius <Vojtech.Vasmucius@student.souepl.cz>
2025-03-07 11:35:26 +0100

    7.3.

--- route/register/register.php
+++ route/register/register.php
@@ -7,5 +7,32 @@
 </head>
 <body>
     <?php include "../../components/header.php" ?>
+
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "prvni_db";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT ID, Název_produktu, Cena_za_kus, Počet_kusů FROM zbozi";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["ID"]. " - Name: " . $row["Název_produktu"]. " " . $row["Cena_za_kus"]. " " . $row["Počet_kusů"]. "<br>";
+        }
+        } else {
+        echo "0 results";
+        }
+        $conn->close();
+    ?>
 </body>
 </html>
