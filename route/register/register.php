86ccc94f356d79280b8cdbe83b310e1a00a07d1e
Vojtěch Vašmucius <Vojtech.Vasmucius@student.souepl.cz>
2025-03-14 11:31:11 +0100

    14.3

--- route/register/register.php
+++ route/register/register.php
@@ -21,18 +21,47 @@
         die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql = "SELECT ID, Název_produktu, Cena_za_kus, Počet_kusů FROM zbozi";
+        $sql = "SELECT * FROM zbozi";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
-        // output data of each row
+            echo "<table>
+                    <tbody>
+                         <tr>
+                            <th>ID</th>
+                            <th>Název produktu</th>
+                            <th>Cena za kus</th>
+                            <th>Počet kusů</th>
+                        </tr>";
+
         while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["ID"]. " - Name: " . $row["Název_produktu"]. " " . $row["Cena_za_kus"]. " " . $row["Počet_kusů"]. "<br>";
+            echo        "<tr>".
+                            "<td>" . $row["ID"]. "</td>".
+                            "<td>" . $row["Název_produktu"]. "</td>".
+                            "<td>" . $row["Cena_za_kus"]. "</td>".
+                            "<td>" . $row["Počet_kusů"]. "</td>".
+                        "</tr>";
         }
+        echo "      </tbody>
+                </table>";
+
         } else {
-        echo "0 results";
+            echo "0 results";
         }
+
         $conn->close();
     ?>
+    <style>
+        table{
+            border: 1px solid black;
+
+            th, td, tr{
+                border: 1px solid black;
+                text-align: center;
+                background-color: red;
+            }
+        }
+
+    </style>
 </body>
 </html>
